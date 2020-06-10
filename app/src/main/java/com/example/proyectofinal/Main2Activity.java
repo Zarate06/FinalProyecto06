52aaccacc383614336f40afd4a1f1ea7557518fc
zarate06 <zarate0616@hotmail.com>
2020-06-10 16:46:08 -0500

    FinalProyecto

---
+++ app/src/main/java/com/example/proyectofinal/Main2Activity.java
@@ -0,0 +1,21 @@
+package com.example.proyectofinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void next(View view) {
+        startActivity(new Intent(getApplicationContext(),Main3Activity.class));
+        finish();
+    }
+}
