f958bc9d643c21279bbc8ab7ad25e8f20e0063c4
zarate06 <zarate0616@hotmail.com>
2020-06-10 23:05:21 -0500

    ultimas_vistas

--- app/src/main/java/com/example/proyectofinal/Main14Activity.java
+++ app/src/main/java/com/example/proyectofinal/Main14Activity.java
@@ -6,15 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class Main13Activity extends AppCompatActivity {
+public class Main14Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main13);
+        setContentView(R.layout.activity_main14);
     }
-    public void next3(View view) {
-        startActivity(new Intent(getApplicationContext(),Main14Activity.class));
+    public void next9(View view) {
+        startActivity(new Intent(getApplicationContext(),Main15Activity.class));
         finish();
     }
 }
